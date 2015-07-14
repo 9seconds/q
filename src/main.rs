cf42e4f6daee3cb51de3b99dbf99f19e6edfefc6
9seconds <nineseconds@yandex.ru>
2015-07-14 12:21:31 +0300

    Add XDG dependenies + command line for the 0.1.0

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,30 @@
+#[macro_use]
 extern crate clap;
 
 
 fn main() {
     let _ = clap::App::new("q")
         .author("Sergey Arkhipov <nineseconds@yandex.ru>")
-        .version("0.1.0")
+        .version(&crate_version!()[..])
         .about("q is a gentle way to grep using predefined regexp sets.")
         .after_help("Please find more documentation at https://github.com/9seconds/q.")
+        .arg(
+            clap::Arg::with_name("same_line")
+                .help("Keep matches on the same line")
+                .short("l")
+                .long("same_line")
+        ).arg(
+            clap::Arg::with_name("RULES_DIRECTORY")
+                .help("Directory where rules could be found. By default it uses $XDG_CONFIG_HOME/q/rules")
+                .short("-r")
+                .long("rules")
+                .takes_value(true)
+        ).arg(
+            clap::Arg::with_name("RULES")
+                .help("Regexp rules to apply to the stdin as a comma-separated list.")
+                .index(1)
+                .required(true)
+        )
         .get_matches();
 
     println!("Hello world")
