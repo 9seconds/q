a1a199b879d3f51f9896addb6e3536c0dfb6ebd5
9seconds <nineseconds@yandex.ru>
2015-07-14 09:12:10 +0300

    Add clap dependency

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+extern crate clap;
+
+
 fn main() {
+    let _ = clap::App::new("q")
+        .author("Sergey Arkhipov <nineseconds@yandex.ru>")
+        .version("0.1.0")
+        .about("q is a gentle way to grep using predefined regexp sets.")
+        .after_help("Please find more documentation at https://github.com/9seconds/q.")
+        .get_matches();
+
     println!("Hello world")
 }
